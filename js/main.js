73a9c8f2044ad4cd72d311e1506e7adad4727dc9
ajelleny <ajelleny@gmail.com>
2021-02-01 16:22:27 -0800

    custom search functionality works

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-// console.log("Sanity Check!")
-
-// // DOM manipulation
-// const giphySearch = document.querySelector('img')
-
-// const button = document.querySelector('button');
-
-
-// button.addEventListener('click', function(evt) {
-//     fetch('https://api.giphy.com/v1/gifs/search?api_key=v6BNp1B7ZvukCjJTE76IuyD9cQXsl3d4')
-//     .then(function(giphyResponse){
-//         return giphyResponse.json();
-//     })
-//     .then(function(newGiphyResponse) {
-//         console.log(newGiphyResponse) 
-//         //DOM manipulation
-//         // giphySearch.innerHTML = newGiphyResponse.
-
-
-//     })
-// })
-
-//ATTEMPT AT FETCHING KEYWORD
-// input.addEventListener('input', function(evt) {
-//     fetch('https://api.giphy.com/v1/gifs/search?api_key=v6BNp1B7ZvukCjJTE76IuyD9cQXsl3d4')
-//     .then(function(giphyInput){
-//         return giphyInput.json();
-//     })
-//     .then(function(newGiphyInput) {
-//         console.log(newGiphyInput) 
-//     })
-// })
-
-
 
 // ATTEMPT AT GETTING A RANDOM GIF TO POPULATE
 // DOM manipulation
