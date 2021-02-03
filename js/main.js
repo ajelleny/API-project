89efa199be498ac5100d55b263cc26399c36fa98
ajelleny <ajelleny@gmail.com>
2021-02-02 19:17:22 -0800

    gif shows up in page

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
 
-// ATTEMPT AT GETTING A RANDOM GIF TO POPULATE
 // DOM manipulation
-const giphySearch = document.querySelector('img')
+// const giphySearch = document.querySelector('p')
+
+// const giphyResults = document.querySelector("img")
+
 
 const button = document.querySelector('button');
 
 
-button.addEventListener('click', function(evt) {
+button.addEventListener('click', (evt) => {
     console.log(document.getElementById("search").value)
 
-        let search = document.getElementById("search").value
-        console.log("this is search", search)
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=v6BNp1B7ZvukCjJTE76IuyD9cQXsl3d4&q=${search}`)
-    .then(function(giphyResponse){
+    let search = document.getElementById("search").value
+    console.log("this is search for", search)
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=v6BNp1B7ZvukCjJTE76IuyD9cQXsl3d4&q=${search}&limit=1`)
+    .then((giphyResponse) => {
         return giphyResponse.json();
     })
-    .then(function(newGiphyResponse) {
-        console.log(newGiphyResponse) 
+    .then((newGiphyResponse) => {
+        console.log(newGiphyResponse)
+        console.log(newGiphyResponse.data[0].url) 
+        //DOM manipulation  
+        // create an image tag set source to results in line 23 - console.log(newGiphyResponse.data[0].url
+        // give it a class to style w/ css
+        // use giphyresults from line 5 and append what i just created to giphy result
+
+
+        let giphyImage = document.createElement('img');
+        //console.log(giphyImage)
+        //giphyImage.setAttribute('src', newGiphyResponse.data[0].url) 
+        giphyImage.setAttribute('src', newGiphyResponse.data[0].images.original.url) 
+        giphyImage.setAttribute('class', "giphyImage") 
+        // append element to container
+        document.getElementById("gif-image").appendChild(giphyImage)
+    
+
+
+        //<img src="newGiphyResponse.data[0].url" alt="">
         
-
-
-        //DOM manipulation
-        // giphySearch.innerHTML = newGiphyResponse.bitly_gif_url
-
-
+        //giphyResults.getElementById('src', newGiphyResponse.data[0].url)
+        
+    
     })
+
 })
 
 
-
+//add button to remove gif from page 
 
